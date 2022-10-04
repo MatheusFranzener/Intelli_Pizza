3d90296ca4d8cd8db768bd2ef4c440b3cd394e4b
AntonioMuller <antonio_brugnago@estudante.sc.senai.br>
2022-10-04 10:57:04 -0300

    Pizzaria Completa(Acho)

--- Scripts/script.js
+++ Scripts/script.js
@@ -18,6 +18,21 @@ function comprarPizza(tamanho) {
         "valor": 0
     }
 
+    switch (tamanho) {
+        case 1:
+            pedido.valor = 20
+            break;
+        case 2:
+            pedido.valor = 30
+            break;
+        case 3:
+            pedido.valor = 40
+            break;
+        case 4:
+            pedido.valor = 50
+            break;
+    }
+
     localStorage.setItem("varPedido", JSON.stringify(numPedido))
     localStorage.setItem('pedido' + numPedido, JSON.stringify(pedido))
 }
@@ -27,33 +42,75 @@ function comprarCombo1() {
 
     pedido = {
         "número": numPedido,
-        "tamanho": 4,
-        "sabores": "mussarela",
+        "tamanho": [4, 2],
+        "sabores": ["Marguerita", "Chocolate Preto"],
         "borda": "",
         "bebida": "",
         "cliente": "",
         "endereço": "",
         "email": "",
         "telefone": "",
-        "valor": 60
+        "valor": 65
     }
 
     localStorage.setItem("varPedido", JSON.stringify(numPedido))
     localStorage.setItem('pedido' + numPedido, JSON.stringify(pedido))
+}
 
+function comprarCombo2() {
     numPedido++
 
     pedido = {
         "número": numPedido,
-        "tamanho": 2,
-        "sabores": "brigadeiro",
+        "tamanho": 3,
+        "sabores": ["Brócolis Especial"],
+        "borda": "",
+        "bebida": "Coca-Cola",
+        "cliente": "",
+        "endereço": "",
+        "email": "",
+        "telefone": "",
+        "valor": 40
+    }
+
+    localStorage.setItem("varPedido", JSON.stringify(numPedido))
+    localStorage.setItem('pedido' + numPedido, JSON.stringify(pedido))
+}
+
+function comprarCombo3() {
+    numPedido++
+
+    pedido = {
+        "número": numPedido,
+        "tamanho": [4, 4],
+        "sabores": ["Calabresa", "Portuguesa"],
+        "borda": "",
+        "bebida": "Guaraná",
+        "cliente": "",
+        "endereço": "",
+        "email": "",
+        "telefone": "",
+        "valor": 85
+    }
+
+    localStorage.setItem("varPedido", JSON.stringify(numPedido))
+    localStorage.setItem('pedido' + numPedido, JSON.stringify(pedido))
+}
+
+function comprarComb4() {
+    numPedido++
+
+    pedido = {
+        "número": numPedido,
+        "tamanho": [4, 3],
+        "sabores": ["Marguerita", "Calabresa"],
         "borda": "",
         "bebida": "",
         "cliente": "",
         "endereço": "",
         "email": "",
         "telefone": "",
-        "valor": 30
+        "valor": 75
     }
 
     localStorage.setItem("varPedido", JSON.stringify(numPedido))
@@ -61,33 +118,75 @@ function comprarCombo1() {
 }
 
 let sabores = []
-
-let nomeCliente = document.querySelector("#nomeCliente").value
-console.log("Nome: ", nomeCliente);
+let valTotal = 0
 
 function fazerPedido() {
+    let saborBorda = document.querySelector('input[name=borda]:checked').value
     let bebida = document.querySelector('input[name=bebida]:checked').value
     let nomeCliente = document.querySelector("#nomeCliente").value
     let emailCliente = document.querySelector("#exampleInputEmail1").value
     let enderecoCliente = document.querySelector("#exampleInputAddress1").value
     let telefoneCliente = document.querySelector("#exampleInputCellphone1").value
 
-    var pedidoTeste = (localStorage.getItem("pedido" + numPedido));
+    var pedidoObj = JSON.parse(localStorage.getItem("pedido" + numPedido))
 
-    pedidoTeste.borda = saborBorda;
-    pedidoTeste.bebida = bebida;
-    pedidoTeste.cliente = nomeCliente;
-    pedidoTeste.email = emailCliente;
-    pedidoTeste.endereço = enderecoCliente;
-    pedidoTeste.telefone = telefoneCliente;
+    if (pedidoObj.borda == "Catupiry") {
+        pedidoObj.valor += 2
+    } else if (pedidoObj.borda == "Chocolate Preto") {
+        pedidoObj.valor += 3
+    }
+
+    if (pedidoObj.bebida == "Coca-Cola") {
+        pedidoObj.valor += 8
+    } else if (pedidoObj.bebida == "Sprite") {
+        pedidoObj.valor += 6
+    } else if (pedidoObj.bebida == "Guaraná") {
+        pedidoObj.valor += 7
+    } else if (pedidoObj.bebida == "Fanta") {
+        pedidoObj.valor += 7
+    }
+
+    pedidoObj.borda = saborBorda
+    if(pedidoObj.sabores == []){
+        pedidoObj.sabores = sabores
+    }
+    if(pedidoObj.bebida == []){
+        pedidoObj.bebida = bebida
+    }
+    pedidoObj.cliente = nomeCliente
+    pedidoObj.email = emailCliente
+    pedidoObj.endereço = enderecoCliente
+    pedidoObj.telefone = telefoneCliente
+
+    localStorage.setItem('pedido' + numPedido, JSON.stringify(pedidoObj))
 }
 
-console.log(localStorage.getItem("pedido" + numPedido))
-
 function addSabor(sabor, n, saborN) {
+    var pedidoObj = JSON.parse(localStorage.getItem("pedido" + numPedido))
+
+    switch (pedidoObj.tamanho) {
+        case 1:
+            if (sabores.length == 1) {
+                return alert("Você já escolheu o máximo de sabores.")
+            }
+        case 2:
+            if (sabores.length == 2) {
+                return alert("Você já escolheu o máximo de sabores.")
+            }
+        case 3:
+            if (sabores.length == 3) {
+                return alert("Você já escolheu o máximo de sabores.")
+            }
+        case 4:
+            if (sabores.length == 4) {
+                return alert("Você já escolheu o máximo de sabores.")
+            }
+    }
+
     document.getElementsByClassName("pointer")[n].removeAttribute("hidden")
     document.getElementsByClassName("pointer")[n + 1].setAttribute("hidden", "hidden")
     document.getElementById("sabor" + saborN).textContent = "1"
+    valTotal += 5
     sabores.push(sabor)
 }
 
@@ -95,6 +194,38 @@ function rmvSabor(sabor, n, saborN) {
     document.getElementsByClassName("pointer")[n].setAttribute("hidden", "hidden")
     document.getElementsByClassName("pointer")[n + 1].removeAttribute("hidden")
     document.getElementById("sabor" + saborN).textContent = "0"
+    valTotal -= 5
     let index = sabores.indexOf(sabor)
     sabores.splice(index, index + 1)
+}
+
+let extra = 0
+
+function entregar() {
+    var pedidoObj = JSON.parse(localStorage.getItem("pedido" + numPedido))
+
+    extra = 5
+    
+    let preco = document.getElementById("preco")
+    preco.textContent = "R$" + (pedidoObj.valor + extra)
+    
+    localStorage.setItem('pedido' + numPedido, JSON.stringify(pedidoObj))
+}
+
+function retirar() {
+    var pedidoObj = JSON.parse(localStorage.getItem("pedido" + numPedido))
+    
+    extra = 5
+
+    let preco = document.getElementById("preco")
+    preco.textContent = "R$" + pedidoObj.valor
+    
+    localStorage.setItem('pedido' + numPedido, JSON.stringify(pedidoObj))
+}
+
+function redirecionar(){
+
+    alert('Seu pedido foi confirmado')
+    localStorage.clear()
+    window.location.href = "http://127.0.0.1:5500/Components/home.html"
 }
